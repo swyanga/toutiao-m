a04f122b504bcc354036dab4a4382d70bcbf0d33
杨思伟 <yangsiwei@yangsiweideMacBook-Pro.local>
2022-05-14 23:19:20 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <!-- 路由的出口 -->
     <router-view/>
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-dianzan"></i>
+    </div>
+    <van-button type="primary">默认按钮</van-button>
   </div>
 </template>
 
